da69b3e369cb900eb562b06a7f7676296e99be3d
Christopher Ly <ChristopherL@datamotion.com>
2018-08-17 16:23:45 -0400

    SDK-29

--- Admin-API-Unit Tests-461/Properties/AssemblyInfo.cs
+++ Admin-API-Unit Tests-461/Properties/AssemblyInfo.cs
@@ -1,20 +1,8 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Admin-API-Unit Tests-461")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Admin-API-Unit Tests-461")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-[assembly: ComVisible(false)]
-
+// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("43a3ab41-dc7a-45cc-a461-82f332665b4b")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
